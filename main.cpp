d2c7971ab62529e37c4a7c748f35df65472cc9d8
Kathrin Korsunska <kkorsunska@gmail.com>
2021-07-09 18:55:11 +0200

    added XXH64 to main

--- main.cpp
+++ main.cpp
@@ -5,9 +5,12 @@
 #include "include/hcrstrat/linear_probing.h"
 #include "include/hash/sph.h"
 #include "include/hashmap/hashmap_scalar.h"
-#include "include/umash/umash.h"
+//#include "include/umash/umash.h"
 #include "include/CSVParser/CSVParser.h"
 #include "include/hash/loadHash.h"
+
+#define XXH_INLINE_ALL
+#include "xxhash.h"
 
 namespace fs = std::filesystem;
 
@@ -30,10 +33,17 @@ int main( ) {
 
     std::cout << "Hello, World!" << std::endl;
 
-    tuddbs::hashmap_scalar_t< tuddbs::hsh, tuddbs::linear_probing_t > hm( 100 );
+    tuddbs::hashmap_scalar_t< tuddbs::hsh, tuddbs::linear_probing_t > hm( csv_contents.size() );
 
-    for( uint64_t i = 0; i < 50; ++i ) {
+    /*for( uint64_t i = 0; i < 50; ++i ) {
         hm.insert( 7*i, i );
+    }*/
+    for(auto elem : csv_contents)
+    {
+        std::string input = elem.second.at(0);
+        const void* inputPointer = input.c_str();
+        XXH64_hash_t hash = XXH64(inputPointer, elem.second.at(0).size(), 0);
+        hm.insert(hash, elem.first);
     }
     for( uint64_t i = 0; i < 50; ++i ) {
         auto [ contains, value_left ] = hm.lookup( 4*i );
